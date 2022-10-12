f8575a629cb260d47966c3c1180c5c32435c646c
Recep Direk <recepdirek@gmail.com>
2022-10-12 10:38:31 +0100

    kubernetes with terraform

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+
+  # other options for authentication
+}
---
+++ terraform.tfvars
@@ -0,0 +1,10 @@
+aws_access_key = "AKIAYQ2RYDDHGOHZONPX"
+aws_secret_key = "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"
+
+region                   = "eu-west-2"
+availability_zones_count = 2
+
+project = "RecepsCluster"
+
+vpc_cidr         = "10.0.0.0/16"
+subnet_cidr_bits = 8
